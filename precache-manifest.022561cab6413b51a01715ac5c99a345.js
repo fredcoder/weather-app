bc9e8a23910fa03acb1e28e6f75b7f264484ad3c
fredcoder <freddy.clavijo@gmail.com>
2020-08-18 22:21:39 +1000

    Updates

---
+++ precache-manifest.022561cab6413b51a01715ac5c99a345.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "028da12b0729402f8da1ee8266128a33",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "737c415711bc48a6fcb6",
+    "url": "/weather/static/css/main.515b6c37.chunk.css"
+  },
+  {
+    "revision": "bfe5452b0058201ed272",
+    "url": "/weather/static/js/2.46d1fa77.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather/static/js/2.46d1fa77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "737c415711bc48a6fcb6",
+    "url": "/weather/static/js/main.11cc0cfa.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  },
+  {
+    "revision": "8bfb2b055ba8ec70fc80fabf1fcf699e",
+    "url": "/weather/static/media/Clear.8bfb2b05.gif"
+  },
+  {
+    "revision": "385c9d14c81bcea74abc36a0eeeef1b1",
+    "url": "/weather/static/media/Clouds.385c9d14.gif"
+  },
+  {
+    "revision": "7b2d27e66e154b24bd9148057a0944fb",
+    "url": "/weather/static/media/Drizzle.7b2d27e6.gif"
+  },
+  {
+    "revision": "1aaee0263ac4706637d132a4d22da726",
+    "url": "/weather/static/media/Rain.1aaee026.gif"
+  },
+  {
+    "revision": "aa108999c225daef3c4baf8a2ed4304d",
+    "url": "/weather/static/media/clouds-few2.aa108999.gif"
+  },
+  {
+    "revision": "16dd23f326812c6a8d8995de5bdee2e6",
+    "url": "/weather/static/media/clouds-few3.16dd23f3.gif"
+  },
+  {
+    "revision": "4be67ce500ee630478d77a7d9bf9599b",
+    "url": "/weather/static/media/pressure.4be67ce5.png"
+  },
+  {
+    "revision": "0456cead743988f3d3357a58f0ca26c8",
+    "url": "/weather/static/media/snow.0456cead.gif"
+  },
+  {
+    "revision": "e0af9b8aa6f1b835f58af1a8d593500b",
+    "url": "/weather/static/media/storm.e0af9b8a.gif"
+  },
+  {
+    "revision": "b6903f6cafe1b64f196bc6c1a98671f3",
+    "url": "/weather/static/media/storm2.b6903f6c.gif"
+  },
+  {
+    "revision": "ac1510ceaa3f560ace371d80205e5631",
+    "url": "/weather/static/media/storm3.ac1510ce.gif"
+  },
+  {
+    "revision": "f9f35b5fc86a5e51efb618df66282dc6",
+    "url": "/weather/static/media/thunder.f9f35b5f.png"
+  },
+  {
+    "revision": "f97d82ed6858467ba71e7f333c21859a",
+    "url": "/weather/static/media/thunderstorm-heavy2.f97d82ed.gif"
+  },
+  {
+    "revision": "e44243fc8782fde21eb0d3aaf62a1a50",
+    "url": "/weather/static/media/thunderstorm-light.e44243fc.gif"
+  },
+  {
+    "revision": "1eea53f0957b8a1a6c40b68e6dfa4f05",
+    "url": "/weather/static/media/visibility.1eea53f0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather/precache-manifest.a1ef1925091014d60c48d6f2a2ddd68d.js"
+  "/weather/precache-manifest.022561cab6413b51a01715ac5c99a345.js"
 );
 
 self.addEventListener('message', (event) => {
